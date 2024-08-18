ca6ece76107bafa7b6918df364fa051a6b7b8e89
SH Emon <emon109848@gmail.com>
2024-08-18 16:19:04 +0600

    Added HelloApiView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test Api View"""
+    def get(self, request, formate=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            'emon',
+            'hridoy',
+            'nihon'
+            ]
+        
+        return Response({'message':'api message', 'an_apiview': an_apiview})
